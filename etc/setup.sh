e7c031055279bb09f3c6bc499c8b8f46ebd21781
Raleigh <RaleighWaters@gmail.com>
2018-04-05 00:15:51 -0400

    - Reverted to using venv instead of venv3

--- etc/setup.sh
+++ etc/setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-export python_bin=/home/raleigh/Development/PyCharmProjects/ICS/venv3/bin/python
+export python_bin=/home/raleigh/Development/PyCharmProjects/ICS/venv/bin/python
+#export python_bin=/opt/Python-2.7.2/bin/python2.7
