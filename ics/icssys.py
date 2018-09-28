398c8a08be23fc05ac704e3f03cac8322396c09b
Raleigh <RaleighWaters@gmail.com>
2018-09-27 22:32:19 -0400

    - Added icssys command for system level control

---
+++ ics/icssys.py
@@ -0,0 +1,32 @@
+import argparse
+import sys
+import network
+from rpcinterface import RPCProxy
+
+
+description_text = ''
+epilog_text = ''
+parser = argparse.ArgumentParser(description=description_text)
+parser.add_argument('-loglevel', help='')
+
+args = parser.parse_args()
+
+if len(sys.argv) <= 1:
+    parser.print_help()
+    exit()
+
+try:
+    conn = network.connect('', 4040)
+except network.ConnectionError:
+    print('Unable to connect to ICS server')
+    exit(1)
+
+rpc_proxy = RPCProxy(conn)
+
+
+if args.loglevel is not None:
+    print(args.loglevel)
+    result = rpc_proxy.set_log_level(args.loglevel)
+
+
+conn.close()
