93a4b395d2a283a873d27b04ed568fec6e3a2d7f
Raleigh <RaleighWaters@gmail.com>
2018-10-23 00:02:39 -0400

    - Simplified exception handling of icssys command - Added function wrapper to proxy calls

--- ics/icssys.py
+++ ics/icssys.py
@@ -4,12 +4,12 @@ import sys
 import network
 from rpcinterface import RPCProxy
 from tabular import print_table
-from ics_exceptions import DoesNotExist
+from ics_exceptions import ICSError
 
 description_text = ''
 epilog_text = ''
 parser = argparse.ArgumentParser(description=description_text)
-parser.add_argument('-loglevel', help='Set system log level')
+parser.add_argument('-loglevel', nargs=1, metavar='<level>', help='Set system log level')
 parser.add_argument('-attr', action='store_true', help='list node attributes')
 parser.add_argument('-value', nargs=1, metavar=('<attr>'),
                     help='print resource  attribute value')
@@ -29,31 +29,30 @@ except network.NetworkConnectionError:
 
 rpc_proxy = RPCProxy(conn)
 
+
+def perform(func, *func_args):
+    try:
+        return func(*func_args)
+    except ICSError as error:
+        print('ERROR: ' + str(error))
+        sys.exit(1)
+
+
 if args.loglevel is not None:
-    result = rpc_proxy.set_log_level(args.loglevel)
+    perform(rpc_proxy.set_log_level, args.loglevel)
 
 elif args.attr:
-    result = rpc_proxy.node_attr()
+    result = perform(rpc_proxy.node_attr)
     print_table(result)
 
 elif args.value is not None:
-    attr = args.value[0]
-    try:
-        result = rpc_proxy.node_value(attr)
-        print(result)
-    except DoesNotExist as error:
-        print('ERROR: ' + str(error))
-        sys.exit(1)
-    except KeyError:
-        print('ERROR: Attribute does not exists')
-        sys.exit(1)
+    attr_name = args.value[0]
+    result = perform(rpc_proxy.node_value, attr_name)
+    print(result)
 
 elif args.modify is not None:
-    attr = args.modify[0]
+    attr_name = args.modify[0]
     value = ' '.join(args.modify[1:])
-    result = rpc_proxy.node_modify(attr, value)
-    if not result:
-        print('ERROR: Attribute does not exists')
-        sys.exit(1)
+    perform(rpc_proxy.node_modify, attr_name, value)
 
 conn.close()
