632fb11338ba0a430a64d5c640fc88f7379a64d5
Raleigh <RaleighWaters@gmail.com>
2021-03-24 18:30:47 -0400

    - Refactored how command is executed and exceptions are caught - Eliminated the use of remote_execute function that prevented catching exceptions - Removed -add and -remove option from icsalert

--- ics/command_line.py
+++ ics/command_line.py
@@ -1,6 +1,5 @@
 import argparse
 import json
-import socket
 import sys
 import time
 from getpass import getuser
@@ -8,82 +7,71 @@ from getpass import getuser
 import Pyro4 as Pyro
 
 from ics.alerts import AlertClient
-from ics.environment import ICS_ENGINE_PORT, ICS_DAEMON_PORT, ICS_ALERT_PORT
 from ics.errors import ICSError
 from ics.tabular import print_table
+from ics.utils import daemon_conn
+from ics.utils import engine_conn
 from ics.utils import ics_version
 from ics.utils import setup_signal_handler, hostname
 
 epilog_text = ''
 
 
-def remote_execute(func, *func_args, **func_kwargs):
-    """Wrapper for running commands remotely"""
-    try:
-        return func(*func_args, **func_kwargs)
-    except ICSError as error:
-        print('ERROR: ' + str(error))
-        sys.exit(1)
-    except Pyro.errors.CommunicationError as err:
-        print('ERROR: Unable to connect to ICS server')
-        print('ERROR: ' + str(err))
-        sys.exit(1)
-    except KeyError as err:
-        print('Error: Unable to connect to ' + str(err))
-        sys.exit(1)
-    except Exception:
-        print("ERROR: An unknown exception occurred.")
-        print('Pyro traceback:')
-        print("".join(Pyro.util.getPyroTraceback()))
-        sys.exit(1)
+def icsstart():
+    execute_command('icsstart')
 
 
-def daemon_conn():
-    uri = 'PYRO:sub_server_control@' + socket.gethostname() + ':' + str(ICS_DAEMON_PORT)
-    return Pyro.Proxy(uri)
+def icsstop():
+    execute_command('icsstop')
 
 
-def engine_conn():
-    uri = 'PYRO:system@' + socket.gethostname() + ':' + str(ICS_ENGINE_PORT)
-    return Pyro.Proxy(uri)
+def icssys():
+    execute_command('icssys')
 
 
-def alert_conn():
-    uri = 'PYRO:alert_handler@' + socket.gethostname() + ':' + str(ICS_ALERT_PORT)
-    return Pyro.Proxy(uri)
+def icsgrp():
+    execute_command('icsgrp')
+
+
+def icsres():
+    execute_command('icsres')
+
+
+def icsalert():
+    execute_command('icsalert')
+
+
+def icsdump():
+    execute_command('icsdump')
 
 
 def comamnd_log(conn, command_name):
     command_args = sys.argv
     command_args.pop(0)
     message = "Host: {}, User: {}, Command: {} {}".format(hostname(), getuser(), command_name, ' '.join(command_args))
-    remote_execute(conn.clus_log_command, message)
+    conn.clus_log_command(message)
 
 
-def icsd():
-    pass
-
-
-def icsstart():
+def command_icsstart():
     setup_signal_handler()
     description_text = 'Start ICS server'
     parser = argparse.ArgumentParser(description=description_text, epilog=epilog_text)
     args = parser.parse_args()
     cluster = daemon_conn()
-    remote_execute(cluster.start)
+    cluster.start()
 
 
-def icsstop():
+def command_icsstop():
     setup_signal_handler()
     description_text = 'Stop ICS server'
     parser = argparse.ArgumentParser(description=description_text, epilog=epilog_text)
     parser.add_argument('-force', action='store_true', help="Force server to stop running")
     args = parser.parse_args()
     cluster = daemon_conn()
-    remote_execute(cluster.stop, args.force)
+    cluster.stop(args.force)
 
 
-def icssys():
+def command_icssys():
     setup_signal_handler()
     description_text = 'Manage ICS system'
     parser = argparse.ArgumentParser(description=description_text, epilog=epilog_text, allow_abbrev=False)
@@ -113,27 +101,27 @@ def icssys():
     comamnd_log(cluster, 'icssys')
 
     if args.add is not None:
-        remote_execute(cluster.add_node, args.add[0])
+        cluster.add_node(args.add[0])
 
     elif args.delete is not None:
-        remote_execute(cluster.delete_node, args.delete[0])
+        cluster.delete_node(args.delete[0])
 
     elif args.loglevel is not None:
-        remote_execute(cluster.set_log_level, args.loglevel[0])
+        cluster.set_log_level(args.loglevel[0])
 
     elif args.list:
-        result = remote_execute(cluster.node_list)
+        result = cluster.node_list()
         result.sort()
         for node in result:
             print(node)
 
     elif args.attr:
-        result = remote_execute(cluster.node_attr)
+        result = cluster.node_attr()
         print_table(result)
 
     elif args.value is not None:
         attr_name = args.value[0]
-        result = remote_execute(cluster.node_value, attr_name)
+        result = cluster.node_value(attr_name)
         print(result)
 
     elif args.modify is not None:
@@ -141,17 +129,17 @@ def icssys():
             attr_name = args.modify[0]
             if secondary_args.append is not None:
                 value = secondary_args.append[0]
-                remote_execute(cluster.node_modify, attr_name, value, append=True)
+                cluster.node_modify(attr_name, value, append=True)
             elif secondary_args.remove is not None:
                 value = secondary_args.remove[0]
-                remote_execute(cluster.node_modify, attr_name, value, remove=True)
+                cluster.node_modify(attr_name, value, remove=True)
             else:
                 print('error: argument -modify: expected use of -append or -remove with 1 argument')
                 sys.exit(1)
         elif len(args.modify) == 2:
             attr_name = args.modify[0]
             value = ' '.join(args.modify[1:])
-            remote_execute(cluster.node_modify, attr_name, value)
+            cluster.node_modify(attr_name, value)
         else:
             parser.print_usage()
             print('error: argument -modify: expected 1 or 2 arguments')
@@ -164,7 +152,7 @@ def icssys():
         parser.print_help()
 
 
-def icsgrp():
+def command_icsgrp():
     setup_signal_handler()
     description_text = 'Manage ICS service groups'
     parser = argparse.ArgumentParser(description=description_text, epilog=epilog_text, allow_abbrev=False)
@@ -211,68 +199,68 @@ def icsgrp():
         group_name = args.online[0]
         if secondary_args.sys is not None:
             system_name = secondary_args.sys[0]
-            remote_execute(cluster.clus_grp_online, group_name, node=system_name)
+            cluster.clus_grp_online(group_name, node=system_name)
         else:
-            remote_execute(cluster.clus_grp_online, group_name)
+            cluster.clus_grp_online(group_name)
 
     elif args.offline is not None:
         group_name = args.offline[0]
         if secondary_args.sys is not None:
             system_name = secondary_args.sys[0]
-            remote_execute(cluster.clus_grp_offline, group_name, node=system_name)
+            cluster.clus_grp_offline(group_name, node=system_name)
         else:
-            remote_execute(cluster.clus_grp_offline, group_name)
+            cluster.clus_grp_offline(group_name)
 
     elif args.add is not None:
         group_name = args.add[0]
-        remote_execute(cluster.clus_grp_add, group_name)
+        cluster.clus_grp_add(group_name)
 
     elif args.delete is not None:
         group_name = args.delete[0]
-        remote_execute(cluster.clus_grp_delete, group_name)
+        cluster.clus_grp_delete(group_name)
 
     elif args.enable is not None:
         group_name = args.enable[0]
-        remote_execute(cluster.clus_grp_enable, group_name)
+        cluster.clus_grp_enable(group_name)
 
     elif args.disable is not None:
         group_name = args.disable[0]
-        remote_execute(cluster.clus_grp_disable, group_name)
+        cluster.clus_grp_disable(group_name)
 
     elif args.enableresources is not None:
         group_name = args.enableresources[0]
-        remote_execute(cluster.clus_grp_enable_resources, group_name)
+        cluster.clus_grp_enable_resources(group_name)
 
     elif args.disableresources is not None:
         group_name = args.disableresources[0]
-        remote_execute(cluster.clus_grp_disable_resources, group_name)
+        cluster.clus_grp_disable_resources(group_name)
 
     elif args.state is not None:
         group_list = args.state  # List of provided group names
         if len(group_list) == 0:
-            results = remote_execute(cluster.clus_grp_state_all)
+            results = cluster.clus_grp_state_all()
             print_table(results)
         elif len(group_list) == 1:
             group_name = group_list[0]
-            group_state = remote_execute(cluster.clus_grp_state, group_name)
+            group_state = cluster.clus_grp_state(group_name)
             print(group_state)
         else:
-            results = remote_execute(cluster.clus_grp_state_all, group_names=group_list)
+            results = cluster.clus_grp_state_all(group_names=group_list)
             print_table(results)
 
     elif args.clear is not None:
         group_name = args.clear[0]
         system_name = args.clear[1]
-        remote_execute(cluster.clus_grp_clear, group_name, system_name)
+        cluster.clus_grp_clear(group_name, system_name)
 
     elif args.flush is not None:
         group_name = args.flush[0]
         system_name = args.flush[1]
-        remote_execute(cluster.clus_grp_flush, group_name, system_name)
+        cluster.clus_grp_flush(group_name, system_name)
 
     elif args.resources is not None:
         group_name = args.resources[0]
-        result = remote_execute(cluster.clus_grp_resources, group_name)
+        result = cluster.clus_grp_resources(group_name)
         result.sort()
         for group_name in result:
             print(group_name)
@@ -284,13 +272,13 @@ def icsgrp():
 
     elif args.attr is not None:
         group_name = args.attr[0]
-        result = remote_execute(cluster.clus_grp_attr, group_name)
+        result = cluster.clus_grp_attr(group_name)
         print_table(result)
 
     elif args.value is not None:
         group_name = args.value[0]
         attr = args.value[1]
-        result = remote_execute(cluster.clus_grp_value, group_name, attr)
+        result = cluster.clus_grp_value(group_name, attr)
         print(result)
 
     elif args.modify is not None:
@@ -299,10 +287,10 @@ def icsgrp():
             attr = args.modify[1]
             if secondary_args.append is not None:
                 value = secondary_args.append[0]
-                remote_execute(cluster.clus_grp_modify, group_name, attr, value, append=True)
+                cluster.clus_grp_modify(group_name, attr, value, append=True)
             elif secondary_args.remove is not None:
                 value = secondary_args.remove[0]
-                remote_execute(cluster.clus_grp_modify, group_name, attr, value, remove=True)
+                cluster.clus_grp_modify(group_name, attr, value, remove=True)
             else:
                 print('error: argument -modify: expected use of -append or -remove with 2 arguments')
                 sys.exit(1)
@@ -311,7 +299,7 @@ def icsgrp():
             group_name = args.modify[0]
             attr = args.modify[1]
             value = ' '.join(args.modify[2:])
-            remote_execute(cluster.clus_grp_modify, group_name, attr, value)
+            cluster.clus_grp_modify(group_name, attr, value)
         else:
             parser.print_usage()
             print('error: argument -modify: expected 2 or 3 arguments')
@@ -335,7 +323,7 @@ def icsgrp():
             timer = -1  # Negative timer means no countdown
 
         while timer != 0:
-            group_states = remote_execute(cluster.clus_grp_state, group_name)
+            group_states = cluster.clus_grp_state(group_name)
 
             if node is not None:
                 if group_states[node] == state_name:
@@ -357,7 +345,7 @@ def icsgrp():
         parser.print_help()
 
 
-def icsres():
+def command_icsres():
     setup_signal_handler()
     description_text = 'Manage ICS resources'
     parser = argparse.ArgumentParser(description=description_text, epilog=epilog_text, allow_abbrev=False)
@@ -404,7 +392,7 @@ def icsres():
         resource_name = args.online[0]
         if secondary_args.sys is not None:
             system_name = secondary_args.sys[0]
-            remote_execute(cluster.clus_res_online, resource_name, system_name)
+            cluster.clus_res_online(resource_name, system_name)
         else:
             print('ERROR: system must be specified.')
             sys.exit(1)
@@ -413,7 +401,7 @@ def icsres():
         resource_name = args.offline[0]
         if secondary_args.sys is not None:
             system_name = secondary_args.sys[0]
-            remote_execute(cluster.clus_res_offline, resource_name, system_name)
+            cluster.clus_res_offline(resource_name, system_name)
         else:
             print('ERROR: system must be specified.')
             sys.exit(1)
@@ -421,60 +409,60 @@ def icsres():
     elif args.add is not None:
         resource_name = args.add[0]
         group_name = args.add[1]
-        remote_execute(cluster.clus_res_add, resource_name, group_name)
+        cluster.clus_res_add(resource_name, group_name)
 
     elif args.delete is not None:
         resource_name = args.delete[0]
-        remote_execute(cluster.clus_res_delete, resource_name)
+        cluster.clus_res_delete(resource_name)
 
     elif args.state is not None:
         resource_list = args.state
         if len(resource_list) == 1:
             resource_name = resource_list[0]
-            resource_state = remote_execute(cluster.clus_res_state, resource_name)
+            resource_state = cluster.clus_res_state(resource_name)
             print(resource_state)
             #results = [[resource_name, resource_state]]
         else:
-            results = remote_execute(cluster.clus_res_state_many, resource_list, include_node=True)
+            results = cluster.clus_res_state_many(resource_list, include_node=True)
             print_table(results)
 
     elif args.link is not None:
         resource_name = args.link[0]
         dependency_name = args.link[1]
-        remote_execute(cluster.clus_res_link, resource_name, dependency_name)
+        cluster.clus_res_link(resource_name, dependency_name)
 
     elif args.unlink is not None:
         resource_name = args.unlink[0]
         dependency_name = args.unlink[1]
-        remote_execute(cluster.clus_res_unlink, resource_name, dependency_name)
+        cluster.clus_res_unlink(resource_name, dependency_name)
 
     elif args.clear is not None:
         resource_name = args.clear[0]
-        remote_execute(cluster.clus_res_clear, resource_name)
+        cluster.clus_res_clear(resource_name)
 
     elif args.probe is not None:
         resource_name = args.probe[0]
-        remote_execute(cluster.clus_res_probe, resource_name)
+        cluster.clus_res_probe(resource_name)
 
     elif args.dep is not None:
-        results = remote_execute(cluster.clus_res_dep, args.dep)
+        results = cluster.clus_res_dep(args.dep)
         header = ['Group', 'Resource', 'Dependency']
         print_table(results, header=header, col_sort=0)
 
     elif args.list is True:
-        resources = remote_execute(cluster.clus_res_list)
+        resources = cluster.clus_res_list()
         for resource in resources:
             print(resource)
 
     elif args.attr is not None:
         resource_name = args.attr[0]
-        result = remote_execute(cluster.clus_res_attr, resource_name)
+        result = cluster.clus_res_attr(resource_name)
         print_table(result)
 
     elif args.value is not None:
         resource_name = args.value[0]
         attr_name = args.value[1]
-        result = remote_execute(cluster.clus_res_value, resource_name, attr_name)
+        result = cluster.clus_res_value(resource_name, attr_name)
         print(result)
 
     elif args.modify is not None:
@@ -487,7 +475,7 @@ def icsres():
             attr = args.modify[1]
             value = ' '.join(args.modify[2:])
 
-        remote_execute(cluster.clus_res_modify, resource_name, attr, value)
+        cluster.clus_res_modify(resource_name, attr, value)
 
     elif args.wait is not None:
         resource_name, state_name = args.wait
@@ -507,7 +495,7 @@ def icsres():
             timer = -1  # Negative timer means no countdown
 
         while timer != 0:
-            resource_states = remote_execute(cluster.clus_res_state, resource_name)
+            resource_states = cluster.clus_res_state(resource_name)
 
             if node is not None:
                 if resource_states[node] == state_name:
@@ -529,15 +517,13 @@ def icsres():
         parser.print_help()
 
 
-def icsalert():
+def command_icsalert():
     setup_signal_handler()
     description_text = 'Manage ICS alerts'
     parser = argparse.ArgumentParser(description=description_text, epilog=epilog_text, allow_abbrev=False)
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-level', nargs=1, metavar='<level>', help='Set system log level')
     group.add_argument('-test', action='store_true', help='')
-    group.add_argument('-add', nargs=1, help='add mail recipient')
-    group.add_argument('-remove', nargs=1, help='remove mail recipient')
     args = parser.parse_args()
 
     if len(sys.argv) <= 1:
@@ -547,21 +533,15 @@ def icsalert():
     if args.level is not None:
         cluster = engine_conn()
         comamnd_log(cluster, 'icsalert')
-        remote_execute(cluster.set_level, args.level[0])
+        cluster.set_level(args.level[0])
     elif args.test:
         alert = AlertClient()
-        remote_execute(alert.test, "This is a test alert.")
-    elif args.add is not None:
-        alert = alert_conn()
-        remote_execute(alert.add_recipient, args.add[0])
-    elif args.remove is not None:
-        alert = alert_conn()
-        remote_execute(alert.remove_recipient, args.remove[0])
+        alert.test("This is a test alert.")
     else:
         parser.print_help()
 
 
-def icsdump():
+def command_icsdump():
     setup_signal_handler()
     description_text = 'Dump system data'
     parser = argparse.ArgumentParser(description=description_text, epilog=epilog_text, allow_abbrev=False)
@@ -570,9 +550,40 @@ def icsdump():
 
     cluster = engine_conn()
     comamnd_log(cluster, 'icsdump')
-    data = remote_execute(cluster.dump)
+    data = cluster.dump()
 
     if args.pretty:
         print(json.dumps(data, indent=4, sort_keys=True))
     else:
         print(json.dumps(data))
+
+
+command_map = {
+    'icsstart': command_icsstart,
+    'icsstop': command_icsstop,
+    'icssys': command_icssys,
+    'icsgrp': command_icsgrp,
+    'icsres': command_icsres,
+    'icsalert': command_icsalert,
+    'icsdump': command_icsdump
+}
+
+
+def execute_command(command):
+    try:
+        func = command_map[command]
+        func()
+    except ICSError as error:
+        print('ERROR: ' + str(error))
+        sys.exit(1)
+    except Pyro.errors.CommunicationError as err:
+        print('ERROR: Unable to connect to ICS server')
+        print('ERROR: ' + str(err))
+        sys.exit(1)
+    except ConnectionRefusedError as err:
+        print('ERROR: Unable to connect to ICS server')
+        print('ERROR: ' + str(err))
+        sys.exit(1)
+    except KeyError as err:
+        print('Error: Unable to connect to ' + str(err))
+        sys.exit(1)
